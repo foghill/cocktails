651bb18ab14a62e2d89c9a6efe1863c4a5197119
Benjamin <ben@thefoghill.com>
2022-06-28 12:14:59 -0400

    searchform input

--- setup/src/components/SearchForm.js
+++ setup/src/components/SearchForm.js
@@ -1,13 +1,24 @@
-import React from 'react'
-import { useGlobalContext } from '../context'
+import React from "react";
+import { useGlobalContext } from "../context";
 
 const SearchForm = () => {
-  const { setSearchTerm } = useGlobalContext()
-  const searchValue = React.useRef('');
-    <div>
-      <h2>search form component</h2>
-    </div>
-  
-}
+  const { setSearchTerm } = useGlobalContext();
+  const searchValue = React.useRef("");
+  const searchCocktail = () => {
+    setSearchTerm(searchValue.current.value)
+  }
 
-export default SearchForm
+  return (
+  <section className="section search">
+    <form className="search-form">
+      <div className="form-control">
+        <label htmlFor="name">Search your favorite cocktail</label>
+        <input type="text" id="name" ref={searchValue}  onChange={searchCocktail}/>
+
+      </div>
+    </form>
+  </section>
+  );
+};
+
+export default SearchForm;
