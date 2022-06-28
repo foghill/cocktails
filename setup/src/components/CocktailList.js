edab220c6949a6281bc3a95408d382117110c0da
Benjamin <ben@thefoghill.com>
2022-06-28 12:39:08 -0400

    cocktail list

--- setup/src/components/CocktailList.js
+++ setup/src/components/CocktailList.js
@@ -1,30 +1,28 @@
-import React from "react";
-import Cocktail from "./Cocktail";
-import Loading from "./Loading";
-import { useGlobalContext } from "../context";
+import React from 'react'
+import Cocktail from './Cocktail'
+import Loading from './Loading'
+import { useGlobalContext } from '../context'
 
-const CocktailList = () => {
-  const { loading, cocktails } = useGlobalContext();
-
+export default function CocktailList() {
+  const { cocktails, loading } = useGlobalContext()
   if (loading) {
-    return <Loading />;
+    return <Loading/>
   }
   if (cocktails.length < 1) {
     return (
-      <h2 className="section-title">No cocktails matched your criteria</h2>
-    );
+      <h2 className='section-title'>
+        no cocktails matched your search criteria
+      </h2>
+    )
   }
-
   return (
-    <section className="section">
-      <h2 className="section-title">cocktails</h2>
-      <div className="cocktail-center">
+    <section className='section'>
+      <h2 className='section-title'>cocktails</h2>
+      <div className='cocktails-center'>
         {cocktails.map((item) => {
-          return <Cocktail key={item.id} {...item} />;
+          return <Cocktail key={item.id} {...item} />
         })}
-        </div>
+      </div>
     </section>
-  );
-};
-
-export default CocktailList;
+  )
+}
