2aa6f0e3aab44ff0e79f713411df635602e1f8f8
Benjamin <ben@thefoghill.com>
2022-06-28 11:17:38 -0400

    context, destructuring json data

--- setup/src/context.js
+++ setup/src/context.js
@@ -9,20 +9,37 @@ const AppProvider = ({ children }) => {
   const [searchTerm, setSearchTerm] = useState("a");
   const [cocktails, setCocktails] = useState([]);
 
-const fetchDrinks = async () => {
-  setLoading(true)
-  try {
-    const response = await fetch(`${url}${searchTerm}`);
-    const data = await response.json();
-    console.log(data)
-  } catch (error) {
-    console.log(error);
-  }
-}
+  const fetchDrinks = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(`${url}${searchTerm}`);
+      const data = await response.json();
+      const { drinks } = data;
+      if (drinks) {
+        const newCocktails = drinks.map((item) => {
+          const { idDrink, strDrink, strDrinkThumb, strAlcoholic, strGlass } =
+            item;
+          return {
+            id: idDrink,
+            name: strDrink,
+            image: strDrinkThumb,
+            alcoholic: strAlcoholic,
+            glass: strGlass,
+          };
+        });
+        setCocktails(newCocktails);
+      } else {
+        setCocktails([]);
+      }
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   useEffect(() => {
     fetchDrinks();
-  },[searchTerm])
+  }, [searchTerm]);
 
   return (
     <AppContext.Provider
