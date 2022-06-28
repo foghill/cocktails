156f311fa3f6ce782109fe3fa48f4620d1f1ef89
Benjamin <ben@thefoghill.com>
2022-06-28 10:41:15 -0400

    cocktail list

--- setup/src/components/CocktailList.js
+++ setup/src/components/CocktailList.js
@@ -4,6 +4,15 @@ import Loading from './Loading'
 import { useGlobalContext } from '../context'
 
 const CocktailList = () => {
+  const { loading, cocktails } = useGlobalContext()
+
+    if (loading) {
+      return <Loading />
+    }
+    if(cocktails.length < 1){
+      return <h2 className='section-title'>No cocktails matched your criteria</h2>
+    }
+
   return (
     <div>
       <h2>cocktail list component</h2>
